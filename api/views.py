42be906faecaaf2969e45034bfc43e2a51d6b433
nsr-g-japan <104160945+nsr-g-japan@users.noreply.github.com>
2024-05-20 14:36:27 +0530

    added HTTP Response

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ environ.Env.read_env()  # reads the .env file
 
 
 # Load database connection details from environment variables
-gl_db_server = env('HOSTNAME')
+gl_db_server = env('HOST_SERVER_NAME')
 gl_db_name = env('HOST_DB_NAME')
 gl_db_username = env('HOST_DB_USERNAME')
 gl_db_password = env('HOST_DB_PASSWORD')
@@ -20,4 +20,4 @@ def get_record(request):
 
     print(gl_db_server, gl_db_username, gl_db_password, gl_db_name)
 
-    return HttpResponse("hello")
+    return HttpResponse(gl_db_server, gl_db_username, gl_db_password, gl_db_name)
