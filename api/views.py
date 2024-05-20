eb1a11610079866a559f2b5badf5eb44b1383944
nsr-g-japan <104160945+nsr-g-japan@users.noreply.github.com>
2024-05-20 15:40:18 +0530

    added api_view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import environ
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 from rest_framework.response import Response
 import os
 
@@ -16,7 +19,7 @@ gl_db_name = env('HOST_DB_NAME')
 gl_db_username = env('HOST_DB_USERNAME')
 gl_db_password = env('HOST_DB_PASSWORD')
 
-
+@api_view(['GET'])
 def get_record(request):
 
     print(gl_db_server, gl_db_username, gl_db_password, gl_db_name)
